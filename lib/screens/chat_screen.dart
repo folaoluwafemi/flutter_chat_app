eea67befd619315799b7cb488bf0288e1d4ca45c
folaoluwafemi <folaoluwafemi55@gmail.com>
2022-03-12 22:13:11 +0100

    changed logo

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -35,149 +35,162 @@ class _ChatScreenState extends State<ChatScreen> {
     String chatName = widget.args.chatName;
 
     return LayoutBuilder(
-      builder: (context, constraints) => SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              icon: const Icon(
-                Icons.arrow_back_ios,
-                color: Colors.white,
-              ),
-            ),
-            title: const Center(
-              child: Text('\u{26A1}Chat'),
-            ),
-          ),
-          body: ModalProgressHUD(
-            inAsyncCall: isPIon,
-            child: SizedBox(
-              height: (MediaQuery.of(context).viewInsets.bottom != 0)
-                  ? (constraints.maxHeight - 80) -
-                      MediaQuery.of(context).viewInsets.bottom
-                  : constraints.maxHeight - 80,
-              child: Stack(
-                children: [
-                  // ListView.builder(
-                  //     itemCount: group.messages.length,
-                  //     itemBuilder: (context, inx) {
-                  //       var message = group.messages[inx];
-                  //       return MessageBox(
-                  //         message: message,
-                  //         isCurrentUser: message.senderEmail == _user!.email!,
-                  //       );
-                  //     }),
-
-                  StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                    stream: _firestore
-                        .collection(chatName)
-                        .orderBy('timeStamp')
-                        .snapshots(),
-                    builder: (context,
-                        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
-                            snapshot) {
-                      if (snapshot.hasError) {
-                        return const Center(
-                            child: Text(
-                                'Error: Firestore stream snapshot has an error'));
-                      }
-                      if (!snapshot.hasData) {
-                        return const CircularProgressIndicator();
-                      }
-
-
-
-                      List<DocumentSnapshot> newSnapshotDocuments =
-                          snapshot.data?.docs ?? [];
-
-                      List<DocumentSnapshot> documentList = List.of(newSnapshotDocuments.reversed);
-
-
-
-                      return SizedBox(
-                        height: (MediaQuery.of(context).viewInsets.bottom != 0)
-                            ? (constraints.maxHeight - 150) -
-                                MediaQuery.of(context).viewInsets.bottom
-                            : constraints.maxHeight - 150,
-                        child: ListView.builder(
-                          reverse: true,
-                          itemCount: documentList.length,
-                          itemBuilder: (context, index) {
-                            MessageModel newMessage = MessageModel.fromSnapshot(
-                                documentList[index]
-                                    as DocumentSnapshot<Map<String, dynamic>>);
-                            return MessageBox(
-                              message: newMessage,
-                              isCurrentUser:
-                                  (newMessage.senderEmail == _user!.email!),
-                            );
-                          },
-                        ),
-                      );
-                    },
+      builder: (context, constraints) =>
+          SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                leading: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
                   ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        border: Border(
-                            top: BorderSide(
-                                color: Colors.blueAccent, width: 2.0)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Flexible(
-                            flex: 4,
-                            child: MessageField(
-                              onTextChanged: (value) {
-                                message = value;
+                ),
+                title: const Center(
+                  child: Text('\u{26A1}Chat'),
+                ),
+              ),
+              body: ModalProgressHUD(
+                inAsyncCall: isPIon,
+                child: SizedBox(
+                  height: (MediaQuery
+                      .of(context)
+                      .viewInsets
+                      .bottom != 0)
+                      ? (constraints.maxHeight - 80) -
+                      MediaQuery
+                          .of(context)
+                          .viewInsets
+                          .bottom
+                      : constraints.maxHeight - 80,
+                  child: Stack(
+                    children: [
+                      // ListView.builder(
+                      //     itemCount: group.messages.length,
+                      //     itemBuilder: (context, inx) {
+                      //       var message = group.messages[inx];
+                      //       return MessageBox(
+                      //         message: message,
+                      //         isCurrentUser: message.senderEmail == _user!.email!,
+                      //       );
+                      //     }),
+
+                      StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                        stream: _firestore
+                            .collection(chatName)
+                            .orderBy('timeStamp')
+                            .snapshots(),
+                        builder: (context,
+                            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                            snapshot) {
+                          if (snapshot.hasError) {
+                            return const Center(
+                                child: Text(
+                                    'Error: Firestore stream snapshot has an error'));
+                          }
+                          if (!snapshot.hasData) {
+                            return const CircularProgressIndicator();
+                          }
+
+
+                          List<DocumentSnapshot> newSnapshotDocuments =
+                              snapshot.data?.docs ?? [];
+
+                          List<DocumentSnapshot> documentList = List.of(
+                              newSnapshotDocuments.reversed);
+
+
+                          return SizedBox(
+                            height: (MediaQuery
+                                .of(context)
+                                .viewInsets
+                                .bottom != 0)
+                                ? (constraints.maxHeight - 150) -
+                                MediaQuery
+                                    .of(context)
+                                    .viewInsets
+                                    .bottom
+                                : constraints.maxHeight - 150,
+                            child: ListView.builder(
+                              reverse: true,
+                              itemCount: documentList.length,
+                              itemBuilder: (context, index) {
+                                MessageModel newMessage = MessageModel
+                                    .fromSnapshot(
+                                    documentList[index]
+                                    as DocumentSnapshot<Map<String, dynamic>>);
+                                return MessageBox(
+                                  message: newMessage,
+                                  isCurrentUser:
+                                  (newMessage.senderEmail == _user!.email!),
+                                );
                               },
                             ),
+                          );
+                        },
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            border: Border(
+                                top: BorderSide(
+                                    color: Colors.blueAccent, width: 2.0)),
                           ),
-                          TextButton(
-                            onPressed: () async {
-                              var newMessage = MessageModel(
-                                  senderEmail: _user!.email!,
-                                  messageText: message,
-                                  timeStamp: DateTime.now());
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Flexible(
+                                flex: 4,
+                                child: MessageField(
+                                  onTextChanged: (value) {
+                                    message = value;
+                                  },
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  var newMessage = MessageModel(
+                                      senderEmail: _user!.email!,
+                                      messageText: message,
+                                      timeStamp: DateTime.now());
 
-                              // await _firestore
-                              //     .collection('chats')
-                              //     .doc(chatDocumentId)
-                              //     .update({
-                              //   'groups.messages.${index + 1}': MessageModel.toJson(newMessage),
-                              // })
-                              // .then((value) => print("user has been updated successfully \n\n\n\n successfully"))
-                              // .catchError((error)=> print('user has NOT been updated due to: $error'));
+                                  // await _firestore
+                                  //     .collection('chats')
+                                  //     .doc(chatDocumentId)
+                                  //     .update({
+                                  //   'groups.messages.${index + 1}': MessageModel.toJson(newMessage),
+                                  // })
+                                  // .then((value) => print("user has been updated successfully \n\n\n\n successfully"))
+                                  // .catchError((error)=> print('user has NOT been updated due to: $error'));
 
-                              await _firestore.collection(chatName).add(
+                                  await _firestore.collection(chatName).add(
                                     MessageModel.toJson(newMessage),
                                   );
-                              setState(() {
-                                _messageController.text = '';
-                              });
-                            },
-                            child: const Text(
-                              'Send',
-                              style: TextStyle(
-                                color: Colors.blueAccent,
-                              ),
-                            ),
-                          )
-                        ],
+                                  setState(() {
+                                    _messageController.text = '';
+                                  });
+                                },
+                                child: const Text(
+                                  'Send',
+                                  style: TextStyle(
+                                    color: Colors.blueAccent,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
     );
   }
 }
